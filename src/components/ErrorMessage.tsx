c5b4198f7c63284e87773fd4b8e4dc869693cfe6
Stephen Mathieson <me@stephenmathieson.com>
2021-01-22 21:06:04 -0500

    Add alert role to error for screen readers (#13)
    This patch adds `role=alert` to our `<ErrorMessage>` component. This helps users of assistive technologies (eg screen readers) understand that _something bad_ happened when the `<ErrorMessage>` is rendered.
    
    See https://www.w3.org/TR/WCAG20-TECHS/ARIA19.html

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -10,7 +10,7 @@ const ErrorMessage: React.ComponentType<Props> = ({
   children,
   title = 'An unexpected error occurred'
 }) => (
-  <div className={styles.error}>
+  <div className={styles.error} role="alert">
     <div className={styles.title}>
       <h4>{title}</h4>
     </div>
