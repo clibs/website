b7d6587d76a5f3a2172aa28c315ad823d357f974
Stephen Mathieson <me@stephenmathieson.com>
2021-08-16 15:19:46 -0400

    Replace syntax highlighter with 5 lines of CSS (#17)
    This patch refactors the `<CodeBlock>` component, removing its reliance on `react-syntax-highlighter`. This greatly reduces the size of the bundle, removes several TypeScript hacks and fixes some security vulnerability warnings.
    
    <details>
    <summary>Build stats with this patch</summary>
    
    ```
    ➜  website git:(remove-syntax-highlighter) NODE_ENV=production yarn build
    yarn run v1.22.10
    $ rimraf dist
    $ parcel build src/index.html
    ✨  Built in 1.38s.
    
    dist/main.a6717af5.js.map     ⚠️  1.09 MB     99ms
    dist/main.a6717af5.js           483.5 KB    1.20s
    dist/main.9ca9ac38.css.map      18.54 KB     11ms
    dist/main.9ca9ac38.css           7.69 KB    845ms
    dist/logo.bc8db1c3.png           1.54 KB     52ms
    dist/favicon.a69d088e.png        1.06 KB     15ms
    dist/index.html                    345 B      9ms
    ✨  Done in 2.46s.
    ➜  website git:(remove-syntax-highlighter)
    ```
    
    </details>
    
    <details>
    <summary>Build stats before this patch</summary>
    
    ```
    ➜  website git:(master) NODE_ENV=production yarn build
    yarn run v1.22.10
    $ rimraf dist
    $ parcel build src/index.html
    ✨  Built in 11.88s.
    
    dist/main.070531fd.js.map     ⚠️  1.19 MB     102ms
    dist/main.070531fd.js          518.89 KB    11.71s
    dist/main.80a5d161.css.map      18.32 KB       6ms
    dist/main.80a5d161.css           7.57 KB    10.63s
    dist/logo.bc8db1c3.png           1.54 KB     1.59s
    dist/favicon.a69d088e.png        1.06 KB     770ms
    dist/index.html                    345 B      15ms
    ✨  Done in 12.97s.
    ➜  website git:(master)
    ```
    
    </details>
    
    We've shaved off ~40k from the bundle and reduced build time by ~500% (12.5s to 2.5s).

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
-// NOTE: `react-syntax-highlighter@v6` is used here (rather than latest) as it works with Parcel.
-// Custom TS definitions are at `/typings/react-syntax-highlighter`.
-import SyntaxHighlighter, {
-  registerLanguage
-} from 'react-syntax-highlighter/light'
-import bash from 'react-syntax-highlighter/languages/hljs/bash'
-import highlighterStyles from 'react-syntax-highlighter/styles/hljs/github'
 import styles from './CodeBlock.css'
-
-registerLanguage('bash', bash)
 
 interface Props {
   source: string
@@ -16,9 +7,9 @@ interface Props {
 
 const CodeBlock: React.ComponentType<Props> = ({ source }) => (
   <div className={styles.code}>
-    <SyntaxHighlighter language="bash" style={highlighterStyles}>
-      {source}
-    </SyntaxHighlighter>
+    <pre>
+      <code>{source}</code>
+    </pre>
   </div>
 )
 
